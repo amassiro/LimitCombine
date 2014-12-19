2074bd109fa10e70cbd1d4017f16345f3f51432a
Andrea <massironi.andrea@gmail.com>
2014-12-19 14:29:48 +0100

    new

---
+++ RunCombineTest.sh
@@ -0,0 +1,3 @@
+text2workspace.py -m 125.6 test.txt                               -P LimitCombine.HiggsWidth.HiggsWidthStandalone:higgswidth  --PO=is2l2nu  -o    test.root
+combine -M MaxLikelihoodFit -t -1 --expectSignal 1  test.root
+python diffNuisances.py -a  mlfit.root -g plots.root   -p CMS_zz4l_GGsm
