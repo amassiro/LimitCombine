2e697a87db5810f378096eb6265cc11f8cfbc97e
Andrea <massironi.andrea@gmail.com>
2015-04-26 12:20:08 +0200

    new

---
+++ HiggsWidth/test/limitForPaper.sh
@@ -0,0 +1,226 @@
+
+
+
+
+# combineCards.py       hww_of0j=datacards-0j/hww-19.36fb.mH125.of_0j_shape.txt \
+#                       >   hwidth_0jet.txt
+# combineCards.py       hww_of1j=datacards-1j/hww-19.36fb.mH125.of_1j_shape.txt \
+#                       >   hwidth_1jet.txt
+# combineCards.py       hww_of2j=datacards-2j/hww-19.36fb.mH125.of_2j_shape.txt \
+#                       >   hwidth_2jet.txt
+# 
+# combineCards.py       hww_of0j=datacards-0j/hww-19.36fb.mH125.of_0j_shape.txt \
+#                       hww_of1j=datacards-1j/hww-19.36fb.mH125.of_1j_shape.txt \
+#                       hww_of2j=datacards-2j/hww-19.36fb.mH125.of_2j_shape.txt \
+#                       >   hwidth_012jet.txt
+# 
+# combineCards.py       hww_of0j=datacards-0j/hww-19.36fb.mH125.of_0j_shape.txt \
+#                       hww_of1j=datacards-1j/hww-19.36fb.mH125.of_1j_shape.txt \
+#                       >   hwidth_01jet.txt
+# 
+# 
+#                       
+#                       
+# combineCards.py       hww_of0j=datacards-0j-7TeV/hww-4.94fb.mH125.of_0j_shape.txt \
+#                       >   hwidth_0jet-7TeV.txt
+# combineCards.py       hww_of1j=datacards-1j-7TeV/hww-4.94fb.mH125.of_1j_shape.txt \
+#                       >   hwidth_1jet-7TeV.txt
+# combineCards.py       hww_of2j=datacards-2j-7TeV/hww-4.94fb.mH125.of_2j_shape.txt \
+#                       >   hwidth_2jet-7TeV.txt
+# 
+# 
+# combineCards.py       hww_of0j=datacards-0j-7TeV/hww-4.94fb.mH125.of_0j_shape.txt \
+#                       hww_of1j=datacards-1j-7TeV/hww-4.94fb.mH125.of_1j_shape.txt \
+#                       hww_of2j=datacards-2j-7TeV/hww-4.94fb.mH125.of_2j_shape.txt \
+#                       >   hwidth_012jet-7TeV.txt
+# 
+# combineCards.py       hww_of0j=datacards-0j-7TeV/hww-4.94fb.mH125.of_0j_shape.txt \
+#                       hww_of1j=datacards-1j-7TeV/hww-4.94fb.mH125.of_1j_shape.txt \
+#                       >   hwidth_01jet-7TeV.txt
+#         
+#          
+# combineCards.py       hww_of0j=datacards-0j/hww-19.36fb.mH125.of_0j_shape.txt \
+#                       hww_of1j=datacards-1j/hww-19.36fb.mH125.of_1j_shape.txt \
+#                       hww_of2j=datacards-2j/hww-19.36fb.mH125.of_2j_shape.txt \
+#                       hww_of0j7TeV=datacards-0j-7TeV/hww-4.94fb.mH125.of_0j_shape.txt \
+#                       hww_of1j7TeV=datacards-1j-7TeV/hww-4.94fb.mH125.of_1j_shape.txt \
+#                       hww_of2j7TeV=datacards-2j-7TeV/hww-4.94fb.mH125.of_2j_shape.txt \
+#                       >   hwidth_012jet-7TeV-8TeV.txt
+#          
+
+         
+         
+         
+combineCards.py       hww_of0j=../../../datacards/HIG-14-032/125/hww-19.36fb.mH125.of_0j_shape.txt \
+                      >   hwidth_0jet.txt
+combineCards.py       hww_of1j=../../../datacards/HIG-14-032/125/hww-19.36fb.mH125.of_1j_shape.txt \
+                      >   hwidth_1jet.txt
+combineCards.py       hww_of2j=../../../datacards/HIG-14-032/125/hww-19.36fb.mH125.of_2j_shape.txt \
+                      >   hwidth_2jet.txt
+
+combineCards.py       hww_of0j=../../../datacards/HIG-14-032/125/hww-19.36fb.mH125.of_0j_shape.txt \
+                      hww_of1j=../../../datacards/HIG-14-032/125/hww-19.36fb.mH125.of_1j_shape.txt \
+                      hww_of2j=../../../datacards/HIG-14-032/125/hww-19.36fb.mH125.of_2j_shape.txt \
+                      >   hwidth_012jet.txt
+
+combineCards.py       hww_of0j=../../../datacards/HIG-14-032/125/hww-19.36fb.mH125.of_0j_shape.txt \
+                      hww_of1j=../../../datacards/HIG-14-032/125/hww-19.36fb.mH125.of_1j_shape.txt \
+                      >   hwidth_01jet.txt
+
+
+                      
+                      
+combineCards.py       hww_of0j=../../../datacards/HIG-14-032/125/hww-4.94fb.mH125.of_0j_shape.txt \
+                      >   hwidth_0jet-7TeV.txt
+combineCards.py       hww_of1j=../../../datacards/HIG-14-032/125/hww-4.94fb.mH125.of_1j_shape.txt \
+                      >   hwidth_1jet-7TeV.txt
+combineCards.py       hww_of2j=../../../datacards/HIG-14-032/125/hww-4.94fb.mH125.of_2j_shape.txt \
+                      >   hwidth_2jet-7TeV.txt
+
+
+combineCards.py       hww_of0j=../../../datacards/HIG-14-032/125/hww-4.94fb.mH125.of_0j_shape.txt \
+                      hww_of1j=../../../datacards/HIG-14-032/125/hww-4.94fb.mH125.of_1j_shape.txt \
+                      hww_of2j=../../../datacards/HIG-14-032/125/hww-4.94fb.mH125.of_2j_shape.txt \
+                      >   hwidth_012jet-7TeV.txt
+
+combineCards.py       hww_of0j=../../../datacards/HIG-14-032/125/hww-4.94fb.mH125.of_0j_shape.txt \
+                      hww_of1j=../../../datacards/HIG-14-032/125/hww-4.94fb.mH125.of_1j_shape.txt \
+                      >   hwidth_01jet-7TeV.txt
+        
+         
+combineCards.py       hww_of0j=../../../datacards/HIG-14-032/125/hww-19.36fb.mH125.of_0j_shape.txt \
+                      hww_of1j=../../../datacards/HIG-14-032/125/hww-19.36fb.mH125.of_1j_shape.txt \
+                      hww_of2j=../../../datacards/HIG-14-032/125/hww-19.36fb.mH125.of_2j_shape.txt \
+                      hww_of0j7TeV=../../../datacards/HIG-14-032/125/hww-4.94fb.mH125.of_0j_shape.txt \
+                      hww_of1j7TeV=../../../datacards/HIG-14-032/125/hww-4.94fb.mH125.of_1j_shape.txt \
+                      hww_of2j7TeV=../../../datacards/HIG-14-032/125/hww-4.94fb.mH125.of_2j_shape.txt \
+                      >   hwidth_012jet-7TeV-8TeV.txt
+         
+
+         
+         
+         
+         
+         
+text2workspace.py -m 125.6 hwidth_0jet.txt        -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nu --PO=MHScaleWW  -o    model_hwwlvlv_0j.root
+text2workspace.py -m 125.6 hwidth_1jet.txt        -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nu --PO=MHScaleWW  -o    model_hwwlvlv_1j.root
+text2workspace.py -m 125.6 hwidth_2jet.txt        -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nu --PO=MHScaleWW  -o    model_hwwlvlv_2j.root
+text2workspace.py -m 125.6 hwidth_012jet.txt      -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nu --PO=MHScaleWW  -o    model_hwwlvlv_012j.root
+text2workspace.py -m 125.6 hwidth_01jet.txt       -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nu --PO=MHScaleWW  -o    model_hwwlvlv_01j.root
+
+
+text2workspace.py -m 125.6 hwidth_0jet-7TeV.txt        -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nu --PO=MHScaleWW  -o    model_hwwlvlv_0j-7TeV.root
+text2workspace.py -m 125.6 hwidth_1jet-7TeV.txt        -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nu --PO=MHScaleWW  -o    model_hwwlvlv_1j-7TeV.root
+text2workspace.py -m 125.6 hwidth_2jet-7TeV.txt        -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nu --PO=MHScaleWW  -o    model_hwwlvlv_2j-7TeV.root
+text2workspace.py -m 125.6 hwidth_012jet-7TeV.txt      -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nu --PO=MHScaleWW  -o    model_hwwlvlv_012j-7TeV.root
+text2workspace.py -m 125.6 hwidth_01jet-7TeV.txt       -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nu --PO=MHScaleWW  -o    model_hwwlvlv_01j-7TeV.root
+
+text2workspace.py -m 125.6 hwidth_012jet-7TeV-8TeV.txt       -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nu --PO=MHScaleWW  -o    model_hwwlvlv_01j-7TeV-8TeV.root
+
+
+
+# 8 TeV
+
+
+combine   -M MultiDimFit model_hwwlvlv_0j.root  --algo=grid --points 2400  -m 125.6               --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Observed.higgsCombineTest.MultiDimFit.0j.StandardModel.root
+
+combine   -M MultiDimFit model_hwwlvlv_0j.root  --algo=grid --points 2400  -t -1 -m 125.6         --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Asimov.higgsCombineTest.MultiDimFit.0j.StandardModel.root
+
+
+combine   -M MultiDimFit model_hwwlvlv_1j.root  --algo=grid --points 2400  -m 125.6              --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Observed.higgsCombineTest.MultiDimFit.1j.StandardModel.root
+
+combine   -M MultiDimFit model_hwwlvlv_1j.root  --algo=grid --points 2400  -t -1 -m 125.6        --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Asimov.higgsCombineTest.MultiDimFit.1j.StandardModel.root
+
+
+combine   -M MultiDimFit model_hwwlvlv_2j.root  --algo=grid --points 2400  -m 125.6              --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Observed.higgsCombineTest.MultiDimFit.2j.StandardModel.root
+
+combine   -M MultiDimFit model_hwwlvlv_2j.root  --algo=grid --points 2400  -t -1 -m 125.6        --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Asimov.higgsCombineTest.MultiDimFit.2j.StandardModel.root
+
+
+combine   -M MultiDimFit model_hwwlvlv_012j.root  --algo=grid --points 2400  -m 125.6              --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Observed.higgsCombineTest.MultiDimFit.012j.StandardModel.root
+
+combine   -M MultiDimFit model_hwwlvlv_012j.root  --algo=grid --points 2400  -t -1 -m 125.6        --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Asimov.higgsCombineTest.MultiDimFit.012j.StandardModel.root
+
+
+combine   -M MultiDimFit model_hwwlvlv_01j.root  --algo=grid --points 2400  -m 125.6              --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Observed.higgsCombineTest.MultiDimFit.01j.StandardModel.root
+
+combine   -M MultiDimFit model_hwwlvlv_01j.root  --algo=grid --points 2400  -t -1 -m 125.6        --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Asimov.higgsCombineTest.MultiDimFit.01j.StandardModel.root
+
+
+
+
+
+
+
+
+
+# 7 TeV
+
+
+
+
+combine   -M MultiDimFit model_hwwlvlv_0j-7TeV.root  --algo=grid --points 2400  -m 125.6               --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Observed.higgsCombineTest.MultiDimFit.0j.StandardModel-7TeV.root
+
+combine   -M MultiDimFit model_hwwlvlv_0j-7TeV.root  --algo=grid --points 2400  -t -1 -m 125.6         --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Asimov.higgsCombineTest.MultiDimFit.0j.StandardModel-7TeV.root
+
+
+combine   -M MultiDimFit model_hwwlvlv_1j-7TeV.root  --algo=grid --points 2400  -m 125.6              --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Observed.higgsCombineTest.MultiDimFit.1j.StandardModel-7TeV.root
+
+combine   -M MultiDimFit model_hwwlvlv_1j-7TeV.root  --algo=grid --points 2400  -t -1 -m 125.6        --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Asimov.higgsCombineTest.MultiDimFit.1j.StandardModel-7TeV.root
+
+
+combine   -M MultiDimFit model_hwwlvlv_2j-7TeV.root  --algo=grid --points 2400  -m 125.6              --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Observed.higgsCombineTest.MultiDimFit.2j.StandardModel-7TeV.root
+
+combine   -M MultiDimFit model_hwwlvlv_2j-7TeV.root  --algo=grid --points 2400  -t -1 -m 125.6        --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Asimov.higgsCombineTest.MultiDimFit.2j.StandardModel-7TeV.root
+
+
+combine   -M MultiDimFit model_hwwlvlv_012j-7TeV.root  --algo=grid --points 2400  -m 125.6              --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Observed.higgsCombineTest.MultiDimFit.012j.StandardModel-7TeV.root
+
+combine   -M MultiDimFit model_hwwlvlv_012j-7TeV.root  --algo=grid --points 2400  -t -1 -m 125.6        --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Asimov.higgsCombineTest.MultiDimFit.012j.StandardModel-7TeV.root
+
+
+combine   -M MultiDimFit model_hwwlvlv_01j-7TeV.root  --algo=grid --points 2400  -m 125.6              --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Observed.higgsCombineTest.MultiDimFit.01j.StandardModel-7TeV.root
+
+combine   -M MultiDimFit model_hwwlvlv_01j-7TeV.root  --algo=grid --points 2400  -t -1 -m 125.6        --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Asimov.higgsCombineTest.MultiDimFit.01j.StandardModel-7TeV.root
+
+
+
+
+
+
+# 7 + 8 TeV
+
+
+combine   -M MultiDimFit model_hwwlvlv_01j-7TeV-8TeV.root  --algo=grid --points 2400  -m 125.6              --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Observed.higgsCombineTest.MultiDimFit.01j.StandardModel-7TeV-8TeV.root
+
+combine   -M MultiDimFit model_hwwlvlv_01j-7TeV-8TeV.root  --algo=grid --points 2400  -t -1 -m 125.6        --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Asimov.higgsCombineTest.MultiDimFit.01j.StandardModel-7TeV-8TeV.root
+
+
+
+
+
+
+
+
