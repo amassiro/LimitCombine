f0e46d1c1b33ff1c718dad6c652d109a826b2f08
Andrea <massironi.andrea@gmail.com>
2015-05-05 15:38:03 +0200

    up

--- HiggsWidth/test/limitForPaper.sh
+++ HiggsWidth/test/limitForPaper.sh
@@ -68,6 +68,11 @@ combineCards.py       hww_of0j=../../../datacards/HIG-14-032/125/hww-19.36fb.mH1
                       >   hwidth_01jet.txt
 
 
+combineCards.py       hww_of0j=../../../datacards/HIG-14-032/125/hww-19.36fb.mH125.of_0j_shape.txt \
+                      hww_of1j=../../../datacards/HIG-14-032/125/hww-19.36fb.mH125.of_1j_shape.txt \
+                      hww_of2j=../../../datacards/HIG-14-032/125/hww-19.36fb.mH125.of_2j_shape.txt \
+                      >   hwidth_012jet_wwlow_wwhigh.txt
+
                       
                       
 combineCards.py       hww_of0j=../../../datacards/HIG-14-032/125/hww-4.94fb.mH125.of_0j_shape.txt \
@@ -146,7 +151,7 @@ mv higgsCombineTest.MultiDimFit.mH125.6.root      Asimov.higgsCombineTest.MultiD
 combine   -M MultiDimFit model_hwwlvlv_012j.root  --algo=grid --points 2400  -m 125.6              --verbose -1
 mv higgsCombineTest.MultiDimFit.mH125.6.root      Observed.higgsCombineTest.MultiDimFit.012j.StandardModel.root
 
-combine   -M MultiDimFit model_hwwlvlv_012j.root  --algo=grid --points 2400  -t -1 -m 125.6        --verbose -1
+combine   -M MultiDimFit model_hwwlvlv_012j.root  --algo=grid --points 2400  -t -1 -m 125.6   --saveToys     --verbose -1
 mv higgsCombineTest.MultiDimFit.mH125.6.root      Asimov.higgsCombineTest.MultiDimFit.012j.StandardModel.root
 
 
@@ -218,6 +223,18 @@ mv higgsCombineTest.MultiDimFit.mH125.6.root      Asimov.higgsCombineTest.MultiD
 
 
 
+combine   -M MultiDimFit model_hwwlvlv_012j-7TeV-8TeV.root  --algo=grid --points 2400  -m 125.6          --minimizerAlgo=Minuit2 --minimizerAlgoForMinos=Minuit2 --minimizerStrategy=1 --minimizerTolerance=0.009 --minimizerToleranceForMinos=0.01 --cminFallbackAlgo Minuit,0.001  --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND     --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root      Observed.higgsCombineTest.MultiDimFit.012j.StandardModel-7TeV-8TeV.magicOptions.root
+
+
+combine   -M MultiDimFit model_hwwlvlv_012j-7TeV-8TeV.root  --algo=grid --points 2400  -m 125.6   --setPhysicsModelParameterRanges CMS_zz4l_GGsm=0.000001,1       --minimizerAlgo=Minuit2 --minimizerAlgoForMinos=Minuit2 --minimizerStrategy=1 --minimizerTolerance=0.009 --minimizerToleranceForMinos=0.01 --cminFallbackAlgo Minuit,0.001  --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND     --verbose -1
+
+
+
+
+
+
+
 
 # get mu
 text2workspace.py -m 125.6 hwidth_012jet-7TeV-8TeV.txt       -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth  --PO=is2l2nuGGsmRVRFfixed  --PO=MHScaleWW  -o    model_hwwlvlv_012j-7TeV-8TeV-RVRFfixed.root
@@ -237,6 +254,25 @@ mv higgsCombineTest.MultiDimFit.mH125.6.root      Observed.higgsCombineTest.Mult
 
 combine   -M MultiDimFit model_hwwlvlv_012j-7TeV-8TeV-RVRFfixed.root  --algo=grid --points 240 -t -1 -m 125.6        --verbose -1
 mv higgsCombineTest.MultiDimFit.mH125.6.root      Asimov.higgsCombineTest.MultiDimFit.012j.StandardModel-7TeV-8TeV.MuScan.root
+
+
+# mu scan 
+
+         
+text2workspace.py -m 125.6 hwidth_0jet.txt        -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nuGGsmRVRFfixed --PO=MHScaleWW  -o    model_hwwlvlv_0j.root
+text2workspace.py -m 125.6 hwidth_1jet.txt        -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nuGGsmRVRFfixed --PO=MHScaleWW  -o    model_hwwlvlv_1j.root
+text2workspace.py -m 125.6 hwidth_2jet.txt        -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nuGGsmRVRFfixed --PO=MHScaleWW  -o    model_hwwlvlv_2j.root
+text2workspace.py -m 125.6 hwidth_012jet.txt      -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nuGGsmRVRFfixed --PO=MHScaleWW  -o    model_hwwlvlv_012j.root
+text2workspace.py -m 125.6 hwidth_01jet.txt       -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nuGGsmRVRFfixed --PO=MHScaleWW  -o    model_hwwlvlv_01j.root
+
+
+text2workspace.py -m 125.6 hwidth_0jet-7TeV.txt        -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nuGGsmRVRFfixed --PO=MHScaleWW  -o    model_hwwlvlv_0j-7TeV.root
+text2workspace.py -m 125.6 hwidth_1jet-7TeV.txt        -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nuGGsmRVRFfixed --PO=MHScaleWW  -o    model_hwwlvlv_1j-7TeV.root
+text2workspace.py -m 125.6 hwidth_2jet-7TeV.txt        -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nuGGsmRVRFfixed --PO=MHScaleWW  -o    model_hwwlvlv_2j-7TeV.root
+text2workspace.py -m 125.6 hwidth_012jet-7TeV.txt      -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nuGGsmRVRFfixed --PO=MHScaleWW  -o    model_hwwlvlv_012j-7TeV.root
+text2workspace.py -m 125.6 hwidth_01jet-7TeV.txt       -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nuGGsmRVRFfixed --PO=MHScaleWW  -o    model_hwwlvlv_01j-7TeV.root
+
+text2workspace.py -m 125.6 hwidth_012jet-7TeV-8TeV.txt       -P LimitCombine.HiggsWidth.HiggsWidthKappaCustodialMHSscaleWWOriginal:higgswidth   --PO=is2l2nuGGsmRVRFfixed --PO=MHScaleWW  -o    model_hwwlvlv_012j-7TeV-8TeV.root
 
 
 
