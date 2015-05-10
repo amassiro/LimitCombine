cd83846a319ae8afdee09fdd8d59d99a3d07d435
Andrea <massironi.andrea@gmail.com>
2015-05-10 11:20:41 +0200

    up

--- HiggsWidth/test/limitForPaper.sh
+++ HiggsWidth/test/limitForPaper.sh
@@ -278,11 +278,29 @@ text2workspace.py -m 125.6 hwidth_012jet-7TeV-8TeV.txt       -P LimitCombine.Hig
 
 
 combine   -M MultiDimFit    model_hwwlvlv_0j.root  --algo=grid --points 240  -m 125.6  --minimizerAlgo=Minuit2 --minimizerAlgoForMinos=Minuit2 --minimizerStrategy=1 --minimizerTolerance=0.009 --minimizerToleranceForMinos=0.01 --cminFallbackAlgo Minuit,0.001  --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND     --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root   Observed.higgsCombineTest.MultiDimFit.0j.StandardModel-8TeV.MuScan.root
+combine   -M MultiDimFit    model_hwwlvlv_0j.root  -t -1  --algo=grid --points 240  -m 125.6  --minimizerAlgo=Minuit2 --minimizerAlgoForMinos=Minuit2 --minimizerStrategy=1 --minimizerTolerance=0.009 --minimizerToleranceForMinos=0.01 --cminFallbackAlgo Minuit,0.001  --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND     --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root   Asimov.higgsCombineTest.MultiDimFit.0j.StandardModel-8TeV.MuScan.root
 
-combine   -M MultiDimFit    model_hwwlvlv10j.root  --algo=grid --points 240  -m 125.6  --minimizerAlgo=Minuit2 --minimizerAlgoForMinos=Minuit2 --minimizerStrategy=1 --minimizerTolerance=0.009 --minimizerToleranceForMinos=0.01 --cminFallbackAlgo Minuit,0.001  --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND     --verbose -1
+combine   -M MultiDimFit    model_hwwlvlv_1j.root  --algo=grid --points 240  -m 125.6  --minimizerAlgo=Minuit2 --minimizerAlgoForMinos=Minuit2 --minimizerStrategy=1 --minimizerTolerance=0.009 --minimizerToleranceForMinos=0.01 --cminFallbackAlgo Minuit,0.001  --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND     --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root   Observed.higgsCombineTest.MultiDimFit.1j.StandardModel-8TeV.MuScan.root
+combine   -M MultiDimFit    model_hwwlvlv_1j.root  -t -1  --algo=grid --points 240  -m 125.6  --minimizerAlgo=Minuit2 --minimizerAlgoForMinos=Minuit2 --minimizerStrategy=1 --minimizerTolerance=0.009 --minimizerToleranceForMinos=0.01 --cminFallbackAlgo Minuit,0.001  --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND     --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root   Asimov.higgsCombineTest.MultiDimFit.1j.StandardModel-8TeV.MuScan.root
 
+combine   -M MultiDimFit    model_hwwlvlv_2j.root  --algo=grid --points 240  -m 125.6  --minimizerAlgo=Minuit2 --minimizerAlgoForMinos=Minuit2 --minimizerStrategy=1 --minimizerTolerance=0.009 --minimizerToleranceForMinos=0.01 --cminFallbackAlgo Minuit,0.001  --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND     --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root   Observed.higgsCombineTest.MultiDimFit.2j.StandardModel-8TeV.MuScan.root
+combine   -M MultiDimFit    model_hwwlvlv_2j.root  -t -1  --algo=grid --points 240  -m 125.6  --minimizerAlgo=Minuit2 --minimizerAlgoForMinos=Minuit2 --minimizerStrategy=1 --minimizerTolerance=0.009 --minimizerToleranceForMinos=0.01 --cminFallbackAlgo Minuit,0.001  --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND     --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root   Asimov.higgsCombineTest.MultiDimFit.2j.StandardModel-8TeV.MuScan.root
 
+combine   -M MultiDimFit    model_hwwlvlv_012j.root  --algo=grid --points 240  -m 125.6  --minimizerAlgo=Minuit2 --minimizerAlgoForMinos=Minuit2 --minimizerStrategy=1 --minimizerTolerance=0.009 --minimizerToleranceForMinos=0.01 --cminFallbackAlgo Minuit,0.001  --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND     --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root   Observed.higgsCombineTest.MultiDimFit.012j.StandardModel-8TeV.MuScan.root
+combine   -M MultiDimFit    model_hwwlvlv_012j.root  -t -1  --algo=grid --points 240  -m 125.6  --minimizerAlgo=Minuit2 --minimizerAlgoForMinos=Minuit2 --minimizerStrategy=1 --minimizerTolerance=0.009 --minimizerToleranceForMinos=0.01 --cminFallbackAlgo Minuit,0.001  --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND     --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root   Asimov.higgsCombineTest.MultiDimFit.012j.StandardModel-8TeV.MuScan.root
 
+combine   -M MultiDimFit    model_hwwlvlv_01j.root  --algo=grid --points 240  -m 125.6  --minimizerAlgo=Minuit2 --minimizerAlgoForMinos=Minuit2 --minimizerStrategy=1 --minimizerTolerance=0.009 --minimizerToleranceForMinos=0.01 --cminFallbackAlgo Minuit,0.001  --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND     --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root   Observed.higgsCombineTest.MultiDimFit.01j.StandardModel-8TeV.MuScan.root
+combine   -M MultiDimFit    model_hwwlvlv_01j.root  -t -1  --algo=grid --points 240  -m 125.6  --minimizerAlgo=Minuit2 --minimizerAlgoForMinos=Minuit2 --minimizerStrategy=1 --minimizerTolerance=0.009 --minimizerToleranceForMinos=0.01 --cminFallbackAlgo Minuit,0.001  --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND     --verbose -1
+mv higgsCombineTest.MultiDimFit.mH125.6.root   Asimov.higgsCombineTest.MultiDimFit.01j.StandardModel-8TeV.MuScan.root
 
 
 
