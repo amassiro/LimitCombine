eeeec848076e36906558689986cfe6c1ad9b0c16
Andrea <massironi.andrea@gmail.com>
2015-05-18 13:42:23 +0200

    -

--- HiggsWidth/test/limitForPaperFC.sh
+++ HiggsWidth/test/limitForPaperFC.sh
@@ -12,9 +12,13 @@ mv higgsCombineTest.HybridNew.mH120.root   higgsCombineTest.HybridNew.01j_8TeV.F
 
 
 
+
+combine model_hwwlvlv_01j.root -M HybridNew --freq --testStat=PL --rule=CLsplusb --singlePoint
+mv higgsCombineTest.HybridNew.mH120.root   higgsCombineTest.HybridNew.01j_8TeV.FC.root
+
+
+
 model_hwwlvlv_012j-7TeV-8TeV.root
-
-
 
 
 
