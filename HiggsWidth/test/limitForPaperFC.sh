86f597078050d4a9a57603955cab7b939025b536
Andrea <massironi.andrea@gmail.com>
2015-05-20 18:38:27 +0200

    script

--- HiggsWidth/test/limitForPaperFC.sh
+++ HiggsWidth/test/limitForPaperFC.sh
@@ -17,6 +17,9 @@ combine model_hwwlvlv_01j.root -M HybridNew --freq --testStat=PL --rule=CLsplusb
 mv higgsCombineTest.HybridNew.mH120.root   higgsCombineTest.HybridNew.01j_8TeV.FC.root
 
 
+
+
+
 combine model_hwwlvlv_012j-7TeV-8TeV.root -M HybridNew --freq --testStat=PL --rule=CLsplusb 
 mv higgsCombineTest.HybridNew.mH120.root   higgsCombineTest.HybridNew.012j_7TeV-8TeV.FC.root
 
@@ -25,5 +28,18 @@ mv higgsCombineTest.HybridNew.mH120.root   higgsCombineTest.HybridNew.012j_8TeV.
 
 
 
+
+
+combine model_hwwlvlv_012j-7TeV-8TeV.root -M FeldmanCousins
+mv higgsCombineTest.HybridNew.mH120.root   higgsCombineTest.HybridNew.012j_7TeV-8TeV.FC.root
+
+
+
+makeGridUsingCrab.py card.txt minimum maximum -n points [other options] -o name
+
+
+python makeGridUsingCrab.py   model_hwwlvlv_012j-7TeV-8TeV.root     0     60     -n  100    -o test      --glidein
+
+python makeGridUsingCrab.py   model_hwwlvlv_012j-7TeV-8TeV.root     0     60     -n  100    -o test      --lsf
 
 
