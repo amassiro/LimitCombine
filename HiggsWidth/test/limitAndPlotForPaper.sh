fc1db633797c53599e4abf9213ab5fdcfe9bd05a
Andrea <massironi.andrea@gmail.com>
2015-06-30 14:14:46 +0200

    update instructions

--- HiggsWidth/test/limitAndPlotForPaper.sh
+++ HiggsWidth/test/limitAndPlotForPaper.sh
@@ -178,11 +178,6 @@ combine   -M MultiDimFit model_hwwlvlv_2j-7TeV-8TeV.root  --algo=grid --points 2
 mv higgsCombineTest.MultiDimFit.mH125.6.root      Asimov.higgsCombineTest.MultiDimFit.2j.StandardModel-7TeV-8TeV.root
 
 
-# combine   -M MultiDimFit model_hwwlvlv_012j-7TeV-8TeV.root  --algo=grid --points 240  -m 125.6          --minimizerAlgo=Minuit2 --minimizerAlgoForMinos=Minuit2 --minimizerStrategy=1 --minimizerTolerance=0.009 --minimizerToleranceForMinos=0.01 --cminFallbackAlgo Minuit,0.001  --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND     --verbose -1
-# mv higgsCombineTest.MultiDimFit.mH125.6.root      Observed.higgsCombineTest.MultiDimFit.012j.StandardModel-7TeV-8TeV.magicOptions.root
-# combine   -M MultiDimFit model_hwwlvlv_012j-7TeV-8TeV.root  --algo=grid --points 240  -m 125.6   --setPhysicsModelParameterRanges CMS_zz4l_GGsm=0.000001,1       --minimizerAlgo=Minuit2 --minimizerAlgoForMinos=Minuit2 --minimizerStrategy=1 --minimizerTolerance=0.009 --minimizerToleranceForMinos=0.01 --cminFallbackAlgo Minuit,0.001  --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND     --verbose -1
-
-
 
          
 
